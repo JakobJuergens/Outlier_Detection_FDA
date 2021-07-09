3aca224339ab47abf2af0b3a5132fed398200319
Juergens <jakob.r.juergens@gmail.com>
2021-07-09 17:03:49 +0200

    zeroing animation
    added zeroing animation

--- auxiliary/observation_vis.R
+++ auxiliary/observation_vis.R
@@ -22,20 +22,72 @@ obs_vis <- function(){
               axis.title.y = element_text(size=18))
 }
 
+zeroing_vis <- function(){
+    tibble_1 <- tibble(x = c(sort(runif(15, 1.5, 5)),
+                             sort(runif(15, 0.5, 3)),
+                             sort(runif(15, 1, 7))),
+                       x_min = c(rep(x[1], times = 15),
+                                 rep(x[16], times = 15),
+                                 rep(x[31], times = 15)),
+                       y = 1.2 * (x-x_min) + rnorm(45, sd = 0.2),
+                       x_max = c(rep(x[15], times = 15),
+                                 rep(x[30], times = 15),
+                                 rep(x[45], times = 15)),
+                       type = c(rep('A', times = 15),
+                                rep('B', times = 15),
+                                rep('C', times = 15)),
+                       arrow_height = c(rep(0, times = 15),
+                                        rep(-0.5, times = 15),
+                                        rep(-1, times = 15)),
+                      shift = rep(FALSE, times = 45))
+    
+    tibble_2 <- tibble(x = tibble_1$x - tibble_1$x_min,
+                       y = tibble_1$y,
+                       x_min = rep(0, times = 45),
+                       x_max = tibble_1$x_max - tibble_1$x_min,
+                       type = c(rep('A', times = 15),
+                                rep('B', times = 15),
+                                rep('C', times = 15)),
+                       arrow_height = tibble_1$arrow_height,
+                      shift = rep(TRUE, times = 45))
+    
+    my_tibble <- rbind(tibble_1, tibble_2)
+    
+    p <- ggplot(data = my_tibble) +
+            geom_point(aes(x = x, y = y, fill = type), col = "blue", shape = 23, size = 5) +
+            ggtitle("Zeroing") +
+            xlab("Angle") + ylab("Torque") +
+            xlim(0, 7) + ylim(-1.5, 7) +
+            geom_vline(aes(xintercept = x_min, colour = type)) +
+            geom_vline(aes(xintercept = x_max, colour = type)) +
+            geom_segment(aes(x = x_min, xend = x_max, y = arrow_height, yend = arrow_height, col = type), arrow = arrow()) +
+            geom_segment(aes(x = x_max, xend = x_min, y = arrow_height, yend = arrow_height, col = type), arrow = arrow()) +
+            theme_light() +
+            theme(plot.title = element_text(size=24),
+                  axis.title.x = element_text(size=18),
+                  axis.title.y = element_text(size=18)) + 
+            transition_states(shift, transition_length = 1, state_length = 1)
+    
+    animate(p, nframes = 400, fps = 40, renderer = gifski_renderer(file = './material/zero.gif'),
+            height = 500, width = 1000)
+    
+}
+
+
 stretching_vis <- function(){
     set.seed(12345)
     
     tibble_1 <- tibble(x = sort(runif(20, 0, 5)), 
-                       y = 1.02*x + rnorm(20, sd = 0.2),
                        x_min = min(x),
+                       y = 1.02*(x-x_min) + rnorm(20, sd = 0.2),
                        x_max = max(x))
     
     x_1_interval <- tibble_1$x_max[1] - tibble_1$x_min[1]
     
     tibble_2u <- tibble(x = sort(runif(20, 0, 4)),
-                        y = 1.03*x + rnorm(20, sd = 0.2),
                         type = "non stretched",
                         x_min = min(x),
+                        y = 1.03*(x - x_min) + rnorm(20, sd = 0.2),
                         x_max = max(x))
     
     x_2_interval <- tibble_2u$x_max[1] - tibble_2u$x_min[1]
